3efa29136567383fc58a0286f8a88b826aa5bea7
David Cox <dave@boundary.com>
2015-01-26 15:21:39 -0600

    Initial version
    Updated start command

---
+++ index.lua
@@ -0,0 +1,20 @@
+local JSON = require('json')
+local fs = require('fs');
+
+local pollInterval;
+
+fs.readFile("param.json", function (err, data)
+---  p("on_read_file", {err=err,data=data})
+  if (err) then return end
+  value = JSON.parse(data)
+  pollInterval = (value['pollInterval'])
+  if (pollInterval == nil) then
+      pollInterval = 1000
+  end
+end)
+
+local timer = require('timer')
+timer.setInterval(1000, function ()
+	x = 5
+	print(string.format("BOUNDARY_LUA_SAMPLE %f %d", math.random(1,10), pollInterval))
+end)
