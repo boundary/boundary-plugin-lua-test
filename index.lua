6eb987020664aac545e35dac57f0e687e883e8d2
David Cox <dave@boundary.com>
2015-01-27 15:31:02 -0600

    Removed some cruft

--- index.lua
+++ index.lua
@@ -4,7 +4,6 @@ local fs = require('fs');
 local pollInterval;
 
 fs.readFile("param.json", function (err, data)
----  p("on_read_file", {err=err,data=data})
   if (err) then return end
   value = JSON.parse(data)
   pollInterval = (value['pollInterval'])
@@ -15,6 +14,5 @@ end)
 
 local timer = require('timer')
 timer.setInterval(1000, function ()
-	x = 5
 	print(string.format("BOUNDARY_LUA_SAMPLE %f", math.random(1,10)))
 end)
