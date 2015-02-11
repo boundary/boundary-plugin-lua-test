24381c8577c5f5884bf9fcce260f30607104d11c
Pearce Barry <pearce@boundary.com>
2015-02-11 15:52:03 -0500

    Update example with boundary module.

--- index.lua
+++ index.lua
@@ -1,24 +1,18 @@
-local JSON = require('json')
-local fs = require('fs');
+local boundary = require('boundary')
 
-local pollInterval;
-local minValue = 1;
-local maxValue = 10;
+local pollInterval = 1000
+local minValue = 1
+local maxValue = 10
 
-fs.readFile("param.json", function (err, data)
-  if (err) then return end
-  value = JSON.parse(data)
-  pollInterval = (value['pollInterval'])
-  if (pollInterval == nil) then
-      pollInterval = 1000
-  end
-  minValue = (value['minValue'])
-  maxValue = (value['maxValue'])
-end)
+if (boundary.param ~= nil) then
+  pollInterval = boundary.param['pollInterval'] or pollInterval
+  minValue = boundary.param['minValue'] or minValue
+  maxValue = boundary.param['maxValue'] or maxValue
+end
 
 print("_bevent:Boundary LUA Sample plugin up : version 1.0|t:info|tags:lua,plugin")
 
 local timer = require('timer')
-timer.setInterval(1000, function ()
+timer.setInterval(pollInterval, function ()
 	print(string.format("BOUNDARY_LUA_SAMPLE %f", math.random(minValue, maxValue)))
 end)
